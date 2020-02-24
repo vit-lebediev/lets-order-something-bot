f2a09a62d26636b4129363d386fed7b4f7520849
Vitalii Lebediev <malgin05@gmail.com>
2020-02-24 16:20:26 +0200

    Update import script: - add kitchens support - add checks for validity of categories and kitchens

--- scripts/parsePlacesCSV.ts
+++ scripts/parsePlacesCSV.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import * as csv from 'fast-csv'; // eslint-disable-line import/no-extraneous-dependencies
 import { Collection } from 'mongodb';
 
+import { FOOD_CATEGORIES, KITCHEN_CATEGORIES } from '../src/Constants';
 import LosMongoClient from '../src/LosMongoClient';
 import Logger from '../src/Logger';
 
@@ -14,6 +15,8 @@ setTimeout(async () => {
 
   await placesCollection.remove({});
 
+  const warnings: string[] = [];
+
   fs.createReadStream(path.resolve(__dirname, '..', 'resources', 'places.csv'))
     .pipe(csv.parse({ headers: true }))
     .on('error', (error) => logger.error(error))
@@ -22,18 +25,43 @@ setTimeout(async () => {
       console.log(row);
 
       const insertRow = row;
-      insertRow.categories = row.categories.split(',');
+
+      if (insertRow.categories !== '') {
+        insertRow.categories = row.categories.split(',');
+
+        for (let i = 0; i < insertRow.categories.length; i += 1) {
+          const cat = insertRow.categories[i];
+
+          if (!(cat in FOOD_CATEGORIES)) {
+            const warningText = `No such category supported: ${ cat }, found in ${ insertRow.name }`;
+            warnings.push(warningText);
+            return;
+          }
+        }
+      }
+
+      if (insertRow.kitchens !== '') {
+        insertRow.kitchens = row.kitchens.split(',');
+
+        for (let i = 0; i < insertRow.kitchens.length; i += 1) {
+          const kitchen = insertRow.kitchens[i];
+
+          if (!(kitchen in KITCHEN_CATEGORIES)) {
+            const warningText = `No such kitchen supported: ${ kitchen }, found in ${ insertRow.name }`;
+            warnings.push(warningText);
+            return;
+          }
+        }
+      }
 
       await placesCollection.insertOne(insertRow);
     })
     .on('end', async (rowCount: number) => {
       logger.info(`Parsed ${ rowCount } rows`);
 
-      logger.info('Reading documents from Mongo collection...');
-
-      const documents = placesCollection.find({});
-
-      console.log(await documents.toArray());
+      for (let i = 0; i < warnings.length; i += 1) {
+        logger.warn(warnings[i]);
+      }
 
       process.exit(0);
     });
